7e7f2b03494d40326895b75204404ba89ef32c38
Ringo <117028634+DarkPhoenix1512@users.noreply.github.com>
2024-10-12 19:37:41 +0200

    Update main.lua

--- client/main.lua
+++ client/main.lua
@@ -1,23 +1,38 @@
 local lastDamage = 0.0
-local curDamage = 0.0
 local vehicle = nil
+local minSpeed = 5.0
+local shakeIntensityDriver = 0.2
+local shakeIntensityPassenger = 0.1
 
 Citizen.CreateThread(function()
     local sleep = 1000
-	while true do
+    while true do
         local playerPed = PlayerPedId()
         if IsPedInAnyVehicle(playerPed) then
             sleep = 100
-            local vehicle = GetVehiclePedIsIn(playerPed, false)
-            local shakeRate = GetEntitySpeed(vehicle) / 250.0 -- 250.0 Normal Shake / 100.0 High Shake / 50.0 Maksimum Shake
+            vehicle = GetVehiclePedIsIn(playerPed, false)
+            local shakeRate = GetEntitySpeed(vehicle)
             local curHealth = GetVehicleBodyHealth(vehicle)
-            if curHealth ~= lastDamage then
-                ShakeGameplayCam("MEDIUM_EXPLOSION_SHAKE", shakeRate)
+            
+            if curHealth ~= lastDamage and shakeRate > minSpeed then
+                local intensity = (shakeRate - minSpeed) / 200.0
+                intensity = math.min(intensity, shakeIntensityDriver)
+                
+                ShakeGameplayCam("SMALL_EXPLOSION_SHAKE", intensity)
+
+                for i = 0, GetVehicleMaxNumberOfPassengers(vehicle) - 1 do
+                    local passengerPed = GetPedInVehicleSeat(vehicle, i)
+                    if passengerPed and passengerPed ~= 0 then
+                        local passengerIntensity = intensity * (shakeIntensityPassenger / shakeIntensityDriver)
+                        ShakeGameplayCam("SMALL_EXPLOSION_SHAKE", passengerIntensity)
+                    end
+                end
             end
+            
             lastDamage = curHealth
         else
             sleep = 1000
         end
         Citizen.Wait(sleep)
-	end
+    end
 end)
